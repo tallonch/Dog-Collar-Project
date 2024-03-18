beb1ffcd3bff76bbaaa4c210e7d7203b2a92eeea
tallonch <tallonchase@gmail.com>
2024-03-18 01:03:03 -0500

    collar_management.dart mockup has been completed. Further adjustments to layout may still be needed to make the final look. Functionality still needs to be added. Add Collar button needs to pull up a dialog/form to enter a new dog collar. Edit Dog collar button needs to both show the information relating to a dog collar, and open a dialog/form to edit said data when pressed.

--- lib/screens/collar_management.dart
+++ lib/screens/collar_management.dart
@@ -6,9 +6,56 @@ class Collar_Management extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return  Scaffold (
-        body: const Center(
-          child: Text('Collar Management Page'),
-        )
+        body: Center(
+          child: Row(
+              children: <Widget>[
+                const Expanded(
+                  flex: 1,
+                  child: Column(),
+                ),
+                Expanded(
+                    flex: 8,
+                    child:
+                      Column(
+                        children: [
+                          ElevatedButton.icon(
+                            onPressed: () {
+                              print("Edit Collar Button Pressed");
+                            },
+                            icon: const Icon(
+                              Icons.edit,
+                              color: Colors.white,
+                              size: 30,
+                            ),
+                            style: ElevatedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(5.0)
+                              ),
+                              minimumSize: Size(double.infinity, 50),
+                            ),
+                            label: Text("Edit Collar"),
+                          ),
+                        ],
+                      ),
+                ),
+                const Expanded(
+                  flex: 1,
+                  child: Column(),
+                ),
+              ]
+          ),
+        ),
+        floatingActionButton: FloatingActionButton.extended(
+          onPressed: () {
+            print("Add Collar Button Pressed");
+          },
+          backgroundColor: Colors.deepPurple,
+          icon: const Icon(Icons.add, color: Colors.white,),
+          label: const Text("Add Collar", style: TextStyle(color: Colors.white)),
+          elevation: 3,
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+
     );
   }
 }
